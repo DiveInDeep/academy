270f9670c9dc984c39853ca737b0a519500d1656
DiveInDeep <kwanbybsv@gmail.com>
2024-09-22 00:23:21 +0800

    DONE [api]: delete course in editCourseForm

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,24 +1,76 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import Mux from "@mux/mux-node";
 import { NextRequest, NextResponse } from "next/server";
 
-export const PATCH = async (req: NextRequest, { params }: { params: { courseId: string } }) => {
+const { video } = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET,
+});
+
+export const PATCH = async (
+  req: NextRequest,
+  { params }: { params: { courseId: string } }
+) => {
   try {
     const { userId } = auth();
     const { courseId } = params;
 
-    if (!userId) { return new Response("Unauthorized", { status: 401 }) }
+    if (!userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
 
     const values = await req.json();
     const course = await db.course.update({
       where: { id: courseId, instructorId: userId },
-      data: { ...values }
-    })
+      data: { ...values },
+    });
 
-    return NextResponse.json(course, { status: 200 })
-
+    return NextResponse.json(course, { status: 200 });
   } catch (err) {
-    console.error(["courseId_PATCH", err])
-    return new Response("Internal Server Error", { status: 500 })
+    console.error(["courseId_PATCH", err]);
+    return new Response("Internal Server Error", { status: 500 });
   }
-}
+};
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { courseId: string } }
+) => {
+  try {
+    const { userId } = auth();
+    const { courseId } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized!", { status: 401 });
+    }
+    const course = await db.course.findUnique({
+      where: { id: courseId, instructorId: userId },
+      include: {
+        sections: {
+          include: {
+            muxData: true,
+          },
+        },
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Course not found", { status: 404 });
+    }
+
+    for (const section of course.sections) {
+      if (section.muxData?.assetId) {
+        await video.assets.delete(section.muxData.assetId);
+      }
+    }
+
+    await db.course.delete({
+      where: { id: courseId, instructorId: userId },
+    });
+    return new NextResponse("Course deleted!", { status: 200 });
+  } catch (err) {
+    console.log("[CouseId_DELETE", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
