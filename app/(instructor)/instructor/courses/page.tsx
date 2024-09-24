d6436711711202b12e1c6e054edf29ccd9cdfc10
DiveInDeep <kwanbybsv@gmail.com>
2024-09-24 21:57:33 +0800

    UPDATE [view]: instructor 's course table

--- app/(instructor)/instructor/courses/page.tsx
+++ app/(instructor)/instructor/courses/page.tsx
@@ -1,3 +1,5 @@
+import { columns } from "@/components/courses/Columns";
+import { DataTable } from "@/components/custom/DataTabe";
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
@@ -25,12 +27,15 @@ const CoursesPage = async () => {
       <Link href="/instructor/create-course">
         <Button>Create New Course</Button>
       </Link>
-      <div className="mt-10">
+      {/* <div className="mt-10">
         {courses.map((course) => (
           <Link href={`/instructor/courses/${course.id}/basic`} key={course.id}>
             {course.title}
           </Link>
         ))}
+      </div> */}
+      <div className="mt-5">
+        <DataTable columns={columns} data={courses} />
       </div>
     </div>
   );
