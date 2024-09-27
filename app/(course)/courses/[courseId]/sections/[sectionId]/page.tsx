378cc0dbb6e8323bddb33af1240041c16ce8f425
DiveInDeep <kwanbybsv@gmail.com>
2024-09-27 16:13:58 +0800

    DONE [view]: course overview page & sections display page

--- app/(course)/courses/[courseId]/sections/[sectionId]/page.tsx
+++ app/(course)/courses/[courseId]/sections/[sectionId]/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const SectionDetailsPage = () => {
+const SectionDetailsPage = async ({
+  params,
+}: {
+  params: {
+    courseId: string;
+    sectionId: string;
+  };
+}) => {
+  const { courseId, sectionId } = params;
+  const { userId } = auth();
+
+  if (!userId) return redirect("/sign-in");
+  const course = db.course.findUnique({
+    where: { id: courseId, isPublished: true },
+    include: {
+      sections: {
+        where: { isPublished: true },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const section = await db.section.findUnique({
+    where: { id: sectionId, courseId, isPublished: true },
+  });
+
+  if (!section) {
+    return redirect(`/courses/${courseId}/overview`);
+  }
+
   return (
-    <div>SectionDetailsPage</div>
-  )
-}
+    <div className="px-6 py-4 flex flex-col gap-5">
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+        <h1 className="text-2xl font-bold max-md:mb-4">{section.title}</h1>
+      </div>
+    </div>
+  );
+};
 
-export default SectionDetailsPage
+export default SectionDetailsPage;
