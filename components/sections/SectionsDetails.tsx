69881c3e0a5ccb23094ebaf1882061a4cac7f220
DiveInDeep <kwanbybsv@gmail.com>
2024-09-28 16:12:57 +0800

    UPDATED [view]: sections details display the content of the section

--- components/sections/SectionsDetails.tsx
+++ components/sections/SectionsDetails.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Course,
@@ -12,7 +12,10 @@ import { Button } from "../ui/button";
 import toast from "react-hot-toast";
 import { useState } from "react";
 import axios from "axios";
-import { Loader2 } from "lucide-react";
+import { File, Loader2, Lock } from "lucide-react";
+import ReadText from "../custom/ReadText";
+import MuxPlayer from "@mux/mux-player-react";
+import Link from "next/link";
 
 interface SectionsDetailsProps {
   course: Course;
@@ -32,6 +35,7 @@ const SectionsDetails = ({
   progress,
 }: SectionsDetailsProps) => {
   const [isLoading, setIsLoading] = useState(false);
+  const isLocked = !purchase && !section.isFree;
 
   const buyCourse = async () => {
     setIsLoading(true);
@@ -60,6 +64,36 @@ const SectionsDetails = ({
         ) : (
           <Button>Mark as Complete</Button>
         )}
+      </div>
+
+      <ReadText value={section.description!} />
+
+      {isLocked ? (
+        <div className="px-10 flex flex-col gap-5 items-center bg-[#FFF8EB]">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm font-bold">
+            Video for this section is locked!. Please buy the course to access
+          </p>
+        </div>
+      ) : (
+        <MuxPlayer
+          playbackId={muxData?.playbackId || ""}
+          className="md: max-w-[600px]"
+        />
+      )}
+
+      <div className="text-xl font-bold mb-5">
+        {resources.map((resource) => (
+          <Link
+            href={resource.fileUrl}
+            key={resource.id}
+            className="flex items-center bg-[#FFF8EB] rounded-lg text-sm font-medium"
+            target="_blank"
+          >
+            <File className="h-4 w-4 mr-4" />
+            {resource.name}
+          </Link>
+        ))}
       </div>
     </div>
   );
